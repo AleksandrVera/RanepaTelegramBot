fa61c8f1793985dfa2e4d0a8ca7d9375ec1bdb38
Aleksandr <151387220+AleksandrVera@users.noreply.github.com>
2023-12-08 12:24:05 +0300

    Delete TelegramBotVeraAleksandr directory

--- TelegramBotVeraAleksandr/methods.py
+++
@@ -1,8 +0,0 @@
-import random 
-
-def greet():
-    return "Приветик!"
-
-def how_are_you():
-  responses = ['Отлично', 'Все плохо:(', 'Ждал этого вопроса от тебя))))']
-  return random.choice(responses)
